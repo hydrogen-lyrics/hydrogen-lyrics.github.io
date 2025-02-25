0544f27f8ce9baa8b9e99a9d28b9f12bf640ee90
SNRainiar <rainiar@foxmail.com>
2025-02-25 18:41:27 +0800

    chore: config basic scaffold

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,24 @@
 import { defineConfig } from '@rsbuild/core';
+
+// Rsbuild plugins
 import { pluginVue } from '@rsbuild/plugin-vue';
 
+// Rspack plugins
+import pluginFont from 'vite-plugin-font';
+
+// Export configs
 export default defineConfig({
+  html: {
+    favicon: './src/assets/favicon.svg',
+    title: 'Hydrogen Lyrics',
+  },
   plugins: [pluginVue()],
+  tools: {
+    bundlerChain(chain, { CHAIN_ID }) {
+      chain.module.rule(CHAIN_ID.RULE.FONT).exclude.add(/\.ttf$/);
+    },
+    rspack: {
+      plugins: [pluginFont.rspack()],
+    },
+  },
 });
